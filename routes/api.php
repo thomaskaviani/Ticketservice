e1fb06dd32382653dd7b8803b06b573845557e60
BartSevenois <bartsevenois123@gmail.com>
2018-09-14 15:56:18 +0200

    test

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,7 @@ use Illuminate\Http\Request;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
-|
+|  WTF IS ER MIS PAE
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
